c889714de5b8eb60cf298959f3b8fdd10114d4eb
bynect <68197565+bynect@users.noreply.github.com>
2024-03-07 21:40:18 +0100

    sus

--- config.h
+++ config.h
@@ -3,27 +3,41 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappih    = 8;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 8;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 8;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 8;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
-static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
-static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const int usealtbar          = 0;        /* 1 means use non-dwm status bar */
+static const char *altbarclass      = ""; /* Alternate bar class name */
+static const char *altbarcmd        = ""; /* Alternate bar launch command */
+static const char *fonts[]          = {
+	"Fira Mono:size=10",
+	"monospace:size=10",
+	"Font Awesome 5 Free:pixelsize=10:antialias=true:autohint=true",
+	"Noto Color Emoji:pixelsize=10:antialias=true:autohint=true",
+	//"JoyPixels:antialias=true:autohint=true",
+};
+static const char dmenufont[]       = "Fira Mono:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#5e5086"; //"#b294bb";//"#005577";
+static const unsigned int baralpha = 240; //0xd8;
+static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg        border     */
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
@@ -35,8 +49,10 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	//{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	//{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ NULL, 	  "qemu",     NULL, 	  0,		    1,			 -1 },
+	//{ NULL,       NULL,       NULL,       0,     		0,           -1 },
 };
 
 /* layout(s) */
@@ -50,22 +66,31 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "[M]",      monocle },
-	{ "[@]",      spiral },
-	{ "[\\]",     dwindle },
-	{ "H[]",      deck },
-	{ "TTT",      bstack },
-	{ "===",      bstackhoriz },
-	{ "HHH",      grid },
-	{ "###",      nrowgrid },
-	{ "---",      horizgrid },
-	{ ":::",      gaplessgrid },
-	{ "|M|",      centeredmaster },
-	{ ">M>",      centeredfloatingmaster },
+	{ "[|]",      tile },    /* first entry is default */
+	{ "[O]",      monocle },
+	{ "[#]",      nrowgrid },
+	{ "[+]",      grid },
 	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ NULL,       NULL },
 };
+
+//static const Layout layouts[] = {
+//	/* symbol     arrange function */
+//	{ "[|]",      tile },    /* first entry is default */
+//	{ "[@]",      spiral },
+//	{ "[\\]",     dwindle },
+//	{ "(O)",      monocle },
+//	//{ "O[]",      deck },
+//	{ "{|}",      bstack },
+//	{ "{-}",      bstackhoriz },
+//	{ "[+]",      grid },
+//	{ "[#]",      nrowgrid },
+//	//{ "|+|",      horizgrid },
+//	//{ "|:|",      gaplessgrid },
+//	{ "[~]",      centeredmaster },
+//	{ "°~.",      centeredfloatingmaster },
+//	{ "°.o",      NULL },    /* no layout function means floating behavior */
+//	{ NULL,       NULL },
+//};
 
 /* key definitions */
 #define MODKEY Mod1Mask
@@ -81,9 +106,14 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
-static const Key keys[] = {
+//static const char *docmenucmd[] = { "docmenu", NULL };
+static const char *utfmenucmd[] = { "utfmenu", NULL };
+
+#include "custom.c"
+
+static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
@@ -95,30 +125,36 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY|Mod4Mask,              XK_u,      incrgaps,       {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_u,      incrgaps,       {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_i,      incrigaps,      {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_i,      incrigaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_o,      incrogaps,      {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_o,      incrogaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_6,      incrihgaps,     {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_6,      incrihgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_7,      incrivgaps,     {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_7,      incrivgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_8,      incrohgaps,     {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_8,      incrohgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_9,      incrovgaps,     {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_9,      incrovgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_Tab,       shiftviewclients, { .i = +1 } },
-	{ MODKEY|ShiftMask,             XK_backslash, shiftviewclients, { .i = -1 } },
+	// GAPS
+	//{ MODKEY|Mod4Mask,              XK_u,      incrgaps,       {.i = +1 } },
+	//{ MODKEY|Mod4Mask|ShiftMask,    XK_u,      incrgaps,       {.i = -1 } },
+	//{ MODKEY|Mod4Mask,              XK_i,      incrigaps,      {.i = +1 } },
+	//{ MODKEY|Mod4Mask|ShiftMask,    XK_i,      incrigaps,      {.i = -1 } },
+	//{ MODKEY|Mod4Mask,              XK_o,      incrogaps,      {.i = +1 } },
+	//{ MODKEY|Mod4Mask|ShiftMask,    XK_o,      incrogaps,      {.i = -1 } },
+	//{ MODKEY|Mod4Mask,              XK_6,      incrihgaps,     {.i = +1 } },
+	//{ MODKEY|Mod4Mask|ShiftMask,    XK_6,      incrihgaps,     {.i = -1 } },
+	//{ MODKEY|Mod4Mask,              XK_7,      incrivgaps,     {.i = +1 } },
+	//{ MODKEY|Mod4Mask|ShiftMask,    XK_7,      incrivgaps,     {.i = -1 } },
+	//{ MODKEY|Mod4Mask,              XK_8,      incrohgaps,     {.i = +1 } },
+	//{ MODKEY|Mod4Mask|ShiftMask,    XK_8,      incrohgaps,     {.i = -1 } },
+	//{ MODKEY|Mod4Mask,              XK_9,      incrovgaps,     {.i = +1 } },
+	//{ MODKEY|Mod4Mask|ShiftMask,    XK_9,      incrovgaps,     {.i = -1 } },
+	//{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
+	//{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
+	//
+	// shiftviewclient
+	//{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY,             			XK_Tab,       shiftviewclients, { .i = +1 } },
+	{ MODKEY,             			XK_backslash, shiftviewclients, { .i = -1 } },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_l,      layoutcycle,    {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -138,11 +174,16 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_r,      quit, /*soft*/  {0} },
 	{ MODKEY|ShiftMask,             XK_q,      quit, /*hard*/  {1} },
+
+	// CUSTOM
+	//{ MODKEY|ShiftMask,             XK_d, spawn,          {.v = docmenucmd } },
+	{ MODKEY|ShiftMask,             XK_p, spawn,          SHCMD("docmenu ~/pub") }, // Normal spawn not expanding ~
+	{ MODKEY|ShiftMask,             XK_u, spawn,          {.v = utfmenucmd } },
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
-static const Button buttons[] = {
+static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
@@ -157,3 +198,21 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 
+//static const char *ipcsockpath = "/tmp/dwm.sock";
+//static IPCCommand ipccommands[] = {
+//  IPCCOMMAND(  view,                1,      {ARG_TYPE_UINT}   ),
+//  IPCCOMMAND(  toggleview,          1,      {ARG_TYPE_UINT}   ),
+//  IPCCOMMAND(  tag,                 1,      {ARG_TYPE_UINT}   ),
+//  IPCCOMMAND(  toggletag,           1,      {ARG_TYPE_UINT}   ),
+//  IPCCOMMAND(  tagmon,              1,      {ARG_TYPE_UINT}   ),
+//  IPCCOMMAND(  focusmon,            1,      {ARG_TYPE_SINT}   ),
+//  IPCCOMMAND(  focusstack,          1,      {ARG_TYPE_SINT}   ),
+//  IPCCOMMAND(  zoom,                1,      {ARG_TYPE_NONE}   ),
+//  IPCCOMMAND(  incnmaster,          1,      {ARG_TYPE_SINT}   ),
+//  IPCCOMMAND(  killclient,          1,      {ARG_TYPE_SINT}   ),
+//  IPCCOMMAND(  togglefloating,      1,      {ARG_TYPE_NONE}   ),
+//  IPCCOMMAND(  setmfact,            1,      {ARG_TYPE_FLOAT}  ),
+//  IPCCOMMAND(  setlayoutsafe,       1,      {ARG_TYPE_PTR}    ),
+//  IPCCOMMAND(  quit,                1,      {ARG_TYPE_NONE}   )
+//};
+//
